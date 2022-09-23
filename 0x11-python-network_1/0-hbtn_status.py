c1f10db05198d1942a98f365b15db22cddbb00b3
Mbicha <mbithicharlse@gmail.com>
2022-09-23 23:44:18 +0300

    Retrieve the url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """
-https://alx-intranet.hbtn.io/status
+retrieve https://alx-intranet.hbtn.io/status
 """
 if __name__ == "__main__":
-    import urllib.request
-
-    alx_req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(alx_req) as ar:
+    import urllib.request as req
+    with req.urlopen('https://alx-intranet.hbtn.io/status') as ar:
         html = ar.read()
         print("Body response:$")
         print(f"\t- type: {type(html)}$")
