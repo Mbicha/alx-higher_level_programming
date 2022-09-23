737125eadef21165df4ea7fafa2dfb6ec70c6939
Mbicha <mbithicharlse@gmail.com>
2022-09-24 00:06:26 +0300

    Sends request and gets back the header id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,6 @@ X-Request-Id variable found in the header of the response.
 if __name__ == "__main__":
     import urllib.request as request
     from sys import argv
-    req =  request.Request(argv[1])
+    req = request.Request(argv[1])
     with request.urlopen(req) as response:
         print(response.headers.get('X-Request-Id'))
